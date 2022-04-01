1b72c43cf4f6bbdbc6d53b77fb2879bda2e9719e
Paulina <91162549+Parteche@users.noreply.github.com>
2022-03-31 20:09:21 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import FilaFocos from "./components/Ejemplo/FilaFocos";
+import FocoMercurial from "./components/Ejemplo/FocoMercurial";
+
+function App() {
+  return (
+  <div id="techo">
+    <FocoMercurial />
+    <FilaFocos />
+    <FocoMercurial />
+  </div>
+  );
+};
+
+export default App;
